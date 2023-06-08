6f3f5156b517e50c0ba932cfff8e4f79e41845ea
Chris Kalafarski <chris@farski.com>
2023-06-08 19:00:40 -0400

    Resolve some DNS

--- src/containers/ffmpeg/ffmpeg.rb
+++ src/containers/ffmpeg/ffmpeg.rb
@@ -12,6 +12,9 @@
 # STATE_MACHINE_ARTIFACT_BUCKET_NAME
 # STATE_MACHINE_FFMPEG_TASK_JSON
 
+require "Resolv"
+p Resolv.getaddress "www.prx.org"
+
 require "aws-sdk-cloudwatch"
 require "aws-sdk-s3"
 require "aws-sdk-sts"
